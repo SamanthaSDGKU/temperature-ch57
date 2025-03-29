c157cf147fd5896bd0be90892a431c70a06e0b4e
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-03-29 14:42:39 -0700

    variables

---
+++ variables.js
@@ -0,0 +1,43 @@
+console.log("Variables and constants");
+//strings
+let myName = "Samantha";
+let lastName = "Jimenez";
+//numbers
+var myAge = 99;
+//boolean (true or false)
+let isStudent = false; 
+
+// display the values
+console.log(myName);
+console.log(lastName);
+
+//concatenation
+console.log("My name is " + myName + " " + lastName);
+
+//operations
+
+let num1=10;
+let num2=20;
+
+let addition = num1 + num2;
+let substraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1/num2;
+
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+
+
+//challenge 1
+
+let a;
+const pi = 3.1416;// can't change
+let r=3;
+
+a = pi * (r*r);
+
+console.log("The results is: " + a);
+
